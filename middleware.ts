a609f91385c3841982f70ffb914e90493da54386
Francis Antwi <sheamusticals@gmail.com>
2025-06-21 03:39:21 +0000

    :initial commit

--- middleware.ts
+++ middleware.ts
@@ -23,7 +23,6 @@ export default withAuth(
 export const config = {
   matcher: [
     "/bookings/:path*",
-    "/listings/:path*",
     "/favourites/:path*",
     "/approvals/:path*",
     "/my-listings/:path*",
