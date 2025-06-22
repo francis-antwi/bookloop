250aa6a59b99d2687c7f0cc3dd809d3093f9f377
Francis Antwi <sheamusticals@gmail.com>
2025-06-22 03:52:14 +0000

    :initial commit

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -76,19 +76,20 @@ const Categories = () => {
             </p>
           </div>
 
-          {/* Categories - Always horizontal */}
-          <div className="relative">
-            <div className="flex gap-3 overflow-x-auto scrollbar-hide scroll-smooth px-1">
-              {categories.map((item) => (
-                <div key={item.label} className="flex-shrink-0">
-                  <CategoryBox
-                    label={item.label}
-                    selected={category === item.label}
-                    icon={item.icon}
-                  />
-                </div>
-              ))}
-            </div>
+          {/* Responsive Category Section */}
+          <div className="flex flex-wrap justify-center gap-3 overflow-x-auto sm:overflow-x-visible scrollbar-hide scroll-smooth px-1">
+            {categories.map((item) => (
+              <div
+                key={item.label}
+                className="flex-shrink-0 w-[140px]"
+              >
+                <CategoryBox
+                  label={item.label}
+                  selected={category === item.label}
+                  icon={item.icon}
+                />
+              </div>
+            ))}
           </div>
 
           {/* Selected Category Info */}
