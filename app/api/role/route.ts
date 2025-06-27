e7b6b46d606dd34f7bef81efc7f9c010cd5e7414
Francis Antwi <sheamusticals@gmail.com>
2025-06-27 21:52:42 +0000

    Making changes

--- app/api/role/route.ts
+++ app/api/role/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import prisma from "@/app/libs/prismadb";
 import { UserRole } from "@prisma/client";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
